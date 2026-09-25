7e03c72f9906b6371c0f3493adeadeaf6362dc62
KaranGade24 <gadekaran24@gmail.com>
2026-09-25 18:12:38 +0530

    style: standardize string quotes in jobViaReferral constants

--- src/api/src/constant/jobViaReferral.constant.js
+++ src/api/src/constant/jobViaReferral.constant.js
@@ -2,13 +2,15 @@
  * JobViaReferral Parser & Integration Constants
  */
 
-export const JOB_VIA_REFERRAL_BASE_URL = 'https://jobviareferral.com';
+export const JOB_VIA_REFERRAL_BASE_URL = "https://jobviareferral.com";
 
 export const JOB_VIA_REFERRAL_CATEGORIES = Object.freeze({
-  HOME: 'https://jobviareferral.com/',
-  FRESHER_REFERRAL: 'https://jobviareferral.com/category/fresher-referral-jobs/',
-  EXPERIENCED_REFERRAL: 'https://jobviareferral.com/category/experienced-referral-jobs/',
-  WORK_FROM_HOME: 'https://jobviareferral.com/category/work-from-home/',
-  QA_REFERRAL: 'https://jobviareferral.com/category/qa-referral-jobs/',
-  ABROAD_JOBS: 'https://jobviareferral.com/category/abroad-jobs/'
+  HOME: "https://jobviareferral.com/",
+  FRESHER_REFERRAL:
+    "https://jobviareferral.com/category/fresher-referral-jobs/",
+  EXPERIENCED_REFERRAL:
+    "https://jobviareferral.com/category/experienced-referral-jobs/",
+  WORK_FROM_HOME: "https://jobviareferral.com/category/remote-jobs/",
+  QA_REFERRAL: "https://jobviareferral.com/category/qa-referral-jobs/",
+  ABROAD_JOBS: "https://jobviareferral.com/category/abroad-jobs/",
 });
